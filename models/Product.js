54e4804c0cd7312319aeb1bc79730524ad3312e9
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-30 15:39:21 -0500

    Updated Product.js to build the foreign key relation to Category table

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,8 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
+const Category = require("./Category");
+
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
@@ -47,5 +49,9 @@ Product.init(
     modelName: 'product',
   }
 );
+
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 module.exports = Product;
