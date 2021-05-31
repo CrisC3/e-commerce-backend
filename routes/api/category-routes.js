a13068cf4f43f4f33bbaa07012f09ea0049dfadb
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-31 15:25:37 -0500

    Added on category-route.js DELETE try-catch barebone statement

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -95,6 +95,11 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
