c9854b1809c9bfb93e53111b55997ba9a67a99e2
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-31 11:51:35 -0500

    Updated GET route for Categories with Products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    
+    const catProdData = await Category.findAll({
+      include: [{model: Product}]
+    });
+
+    res.status(200).json(catProdData);
+
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
